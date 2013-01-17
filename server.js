bfeffad96e39ab15ddb1b6bb8170dc2c7885686f
bluishoul <bluishoul@gmail.com>
2013-01-17 13:23:31 +0800

    xx

--- server.js
+++ server.js
@@ -37,6 +37,7 @@ var server = http.createServer(function(request, response){
 				    };
 					response.write(file, "binary");
 					response.end();
+                    
                 }
             });
         }
