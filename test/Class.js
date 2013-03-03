8eb72179cda2bf310faab99bfff2d5af0e69dd9e
bluishoul <bluishoul@gmail.com>
2013-03-03 23:47:10 +0800

    Start Using Junior

--- test/Class.js
+++ test/Class.js
@@ -1,45 +1,43 @@
-var Class = (function(){
- 
-    var instance,               //当前类内部引用实例
-        interfaces = [],        //需要实现的接口
-        instance_list = {},     //所有实例
-        className = "Class";    //当前类类名
- 
+var Class = (function() {
+
+    var instance, //当前类内部引用实例
+        interfaces = [], //需要实现的接口
+        instance_list = {}, //所有实例
+        className = "Class"; //当前类类名
+
     /**
      *  构造函数
      *  class_name : 类名，字符串
      *  implements : 类实现，JSON对象
      */
-    function Class(class_name,implements){
+
+    function Class(class_name, implements) {
         instance = this;
-        init.call(this,class_name,implements);
+        init.call(this, class_name, implements);
         return this;
     }
- 
+
     /**
      *  类参数初始化，检查类名和实现是否为空
      *  class_name : 类名，字符串
      *  impls : 类实现，JSON对象
      */
-    var init = function(class_name,impls){
-        if(isEmpty(class_name))
-            Console.error.call(this,"ClassName is Null!");
-        else
-            instance.className = class_name;
-        if(isEmpty(impls))
-            Console.error.call(this,"Implements of ["+instance.className+"] is Null!");
-        else
-            instance.implements = impls;
+    var init = function(class_name, impls) {
+        if (isEmpty(class_name)) Console.error.call(this, "ClassName is Null!");
+        else instance.className = class_name;
+        if (isEmpty(impls)) Console.error.call(this, "Implements of [" + instance.className + "] is Null!");
+        else instance.implements = impls;
     };
- 
+
     /**
      *  实例化当前类
      */
-    Class.prototype.instance = function(){
+    Class.prototype.instance = function() {
         instance = this;
         var cur = addIntance(instance.implements);
         delete cur.interface;
         delete cur.instance;
+        delete cur.implements;
         return cur;
     };
 
@@ -47,107 +45,111 @@ var Class = (function(){
      *  将新实例化对象加入到对象列表中
      *  ins : 实例化的对象
      */
-    var addIntance = function(ins){
+    var addIntance = function(ins) {
         var temp = {};
-        $.extend(temp,ins);
+        $.extend(temp, ins);
         var timestamp = new Date().getTime();
-        var key = instance.className+'#'+timestamp;
-        if(isNotEmpty(get(key))){
+        var key = instance.className + '#' + timestamp;
+        if (isNotEmpty(get(key))) {
             return addIntance(ins);
         }
-        temp.id = function(){return key;};
+        temp.id = function() {
+            return key;
+        };
         instance_list[key] = temp;
         return temp;
     };
- 
+
     /**
      *  从对象列表中移除对象
      *  key : 对象ID
      */
-    var removeInstance = function(key){
-        if(isNotEmpty(instance_list[key]))
-            instance_list[key] = undefined;
+    var removeInstance = function(key) {
+        if (isNotEmpty(instance_list[key])) instance_list[key] = undefined;
     };
- 
+
     /**
      *  从对象列表中获取对象
      *  key ：对象ID
      */
-    var get = Class.get = Class.prototype.get = function(key){
-        if(isEmpty(key))return;
+    var get = Class.get = Class.prototype.get = function(key) {
+        if (isEmpty(key)) return;
         return instance_list[key];
     };
-     
+
     /**
      *  检查当前类是否实现了所有接口中的方法
      *  ins ：当前类的实现
      */
-    var checkInterfaceImplements = function(ins){
+    var checkInterfaceImplements = function(ins) {
         var i = 0;
         var errs = [];
-        for(var i=0;i<interfaces.length;i++){
+        for (var i = 0; i < interfaces.length; i++) {
             var Interface = interfaces[i];
-            $.each(Interface,function(name,method){
-                if(name!=="name" && isNotFunc(ins[name])){
-                    errs.push(ins.getClassName()+" didnot implements method ["+name+"] in the interface ["+Interface.name+"]!");
+            $.each(Interface, function(name, method) {
+                if (name !== "name" && isNotFunc(ins[name])) {
+                    errs.push(ins.getClassName() + " didnot implements method [" + name + "] in the interface [" + Interface.name + "]!");
                 }
-            });     
+            });
         }
-        if(errs.length>0)
-            Console.error.call(instance,errs);
-        return errs.length==0;
+        if (errs.length > 0) {
+            Console.error.call(instance, errs);
+        }
+        return errs.length == 0;
     };
- 
+
     /**
      *  获取当前类名
      */
-    Class.prototype.getClassName = function(){
+    Class.prototype.getClassName = function() {
         return instance.className;
     };
- 
+
     /**
      * 设置类需要实现的接口
      * 参数：JSON格式的接口
      */
-    Class.prototype.interface = function(){
+    Class.prototype.interface = function() {
         instance = this;
         var Interfaces = arguments;
-        $.each(arguments,function(i,v){
+        $.each(arguments, function(i, v) {
             interfaces.push(v);
         });
-        if(isNotEmpty(get(this.id))){
-            Console.error.call(this,"Can`t instance "+this.getClassName()+" before implements the interface!");
+        if (isNotEmpty(get(this.id))) {
+            Console.error.call(this, "Can`t instance " + this.getClassName() + " before implements the interface!");
             return;
         }
-        if(isNotEmpty(instance.implements)){
+        if (isNotEmpty(instance.implements)) {
             var constructor = instance.implements[instance.getClassName()];
             var ins = isFunc(constructor) && new constructor();
             var impl_methods = {};
-            $.each(instance.implements,function(index,value){
-                if(index != instance.getClassName())
-                    impl_methods[index] = value;
+            $.each(instance.implements, function(index, value) {
+                if (index != instance.getClassName()) impl_methods[index] = value;
             });
-            if(!ins)
-                ins = {};
-            $.extend(ins,impl_methods);
-            instance.implements = ins ? $.extend(ins,this) : instance.implements;
+            if (!ins) ins = {};
+            $.extend(ins, impl_methods);
+            instance.implements = ins ? $.extend(ins, this) : instance.implements;
             //check interface implements;
             var errors = [];
             for (var i = 0; i < interfaces.length; i++) {
                 var itf = interfaces[i];
-                $.merge(errors,itf.checkInterfaceImplements(instance.implements));
+                $.merge(errors, itf.checkInterfaceImplements(instance.implements));
             };
-            if(errors.length>0){
-                Console.error.call(ins,errors);
+            if (errors.length > 0) {
+                Console.error.call(ins, errors);
                 return;
             }
-       }
+        }
+        interfaces = [];
         return this;
     }
 
-    Class.prototype.extends = function(ins){
-        $.extend(this,ins);
-    };
- 
+
+    /*  有待考虑
+        Class.prototype.extends = function(ins) {
+            $.extend(ins,this);
+            $.extend(this,ins);
+        };
+    */
     return Class;
 })();
--- test/test.js
+++ test/test.js
@@ -1,10 +1,3 @@
-var IMachine = new Interface("Machine",{
-	methods:["ruin"],
-	props:{
-		"name":"hello",
-		"product_id":12312
-	}
-});
 
 var IAnimal = new Interface("Animal",{
 	methods:["eat","shut"],
@@ -14,15 +7,18 @@ var IAnimal = new Interface("Animal",{
 	}
 });
 
+var IMachine = new Interface("Machine",{
+	methods:["ruin"],
+	props:{
+		"name":"hello",
+		"product_id":12312
+	}
+});
+
+
 var Man = new Class("Man",{
 	eat:function(){
-		alert("I am eating!");
-	},
-	speak:function(){
-		alert("I am speaking!");
-	},
-	ruin:function(){
-		alert("I can ruin you!");
+		alert("I am eating!"+this.name+"#"+this.height+"#"+this.id());
 	},
 	shut:function(){
 		alert("shut up!");
@@ -30,16 +26,18 @@ var Man = new Class("Man",{
 }).interface(IAnimal);
 
 var Boy = new Class("Boy",{
-
-});
+	ruin:function(){
+		alert("I can ruin you!");
+	}
+}).interface(IMachine);
 
 var man = Man.instance();
 var man1 = Man.instance();
 var boy = Boy.instance();
 var boy1 = Boy.instance();
 
-Console.log(man.id());
-Console.log(man1.id());
-Console.log(boy.id());
-Console.log(boy1.id());
+Console.log(man);
+Console.log(man1);
+Console.log(boy);
+Console.log(boy1);
 
